0bab723816934de996949c000cf6c930dfe66fef
Alex B <quantbin@users.noreply.github.com>
2016-07-22 16:56:32 -0400

    ast

--- config.fs
+++ config.fs
@@ -1,60 +1,41 @@
 module config
-//system
-type File=System.IO.File
-type Math=System.Math
-type Directory=System.IO.Directory
+//system types
 type Console=System.Console
-type Marshal=System.Runtime.InteropServices.Marshal
+type Math=System.Math
 type Type=System.Type
-//interop
-type Application=Microsoft.Office.Interop.Excel.Application
-type Worksheet=Microsoft.Office.Interop.Excel.Worksheet
-type Range=Microsoft.Office.Interop.Excel.Range
-type ApplicationClass=Microsoft.Office.Interop.Excel.ApplicationClass
-type XlReferenceStyle=Microsoft.Office.Interop.Excel.XlReferenceStyle
-type XlLookAt=Microsoft.Office.Interop.Excel.XlLookAt
-type XlSearchDirection=Microsoft.Office.Interop.Excel.XlSearchDirection
-type XlFindLookIn =Microsoft.Office.Interop.Excel.XlFindLookIn 
-//types
-type Proc={arg:string list;body:string}
-type Rng={top:int;left:int;width:int;height:int}
-type Mtx={mtxData:obj [,]}
-type KitchenSink=
-    |F of float
-    |I of int
-    |S of string
-    |R of Rng
-    |M of Mtx //matrix
-    |C of KitchenSink list //collection of kitchen sink objects
-    |GV of string
-    |LV of string
-    |EmptyGV of string
-    |EmptyLV of string
-    |EmptySink
-//globals
+type File=System.IO.File
+type Directory=System.IO.Directory
+type Dictionary<'a,'b>=System.Collections.Generic.Dictionary<'a,'b>
+type IList=System.Collections.IList
+type DateTime=System.DateTime
+//app types
+type Literal=
+    |BOOL of bool
+    |INT of int
+    |FLOAT of float
+    |STR of string
+    |TIME of DateTime
+type Expr=
+    //tokens - literal
+    |LITERAL of Literal
+    //tokens - vars
+    |VAR of string
+    |VARIDX of string*int
+    |VARMAP of string*string
+    |DOTS of int
+    |DOTSIDX of int*int
+    |DOTSMAP of int*string
+    //tokens - lambda
+    |LAM of string list*Expr
+    //expression - list of tokens and start/end indexes
+    |COMPOSITE of Expr list*int*int
+//global vars:
+//bindings
+let mutable SCOPES:List<Dictionary<string,Expr>>=[]//we will be moving up/down this list; keep scopes in place, just reset them to speed things up (up to max # of scopes)
 let HELP="
-EXIT            exit shell
-HIST            show commands history
-NEW             start new Excel instance
-CELL 1 1        create reference to cell
-RANG 1 1 2 2    create reference to range
-NEW             open new excel workbook
--> n v
-<- v n          assignment
-PRN $x          print var $x
-+-*/            arithmetic
-@ $x            value of var $x
->R N            move range N steps to the right (or left if N is negative)
->R N            move range N steps down (or up if N is negative)
-? ...           search for string in active sheet; returns collection of matched cells
-...\...         eval left side - it should return a collection; then exec right side on each elem of collection
+\exit            exit shell
+\hist            show commands history
 "
 let mutable HIST=[""]
-let mutable EXCEL:Application=null
 let mutable DONE=false
-///map of local variables that are valid for current line only
-let mutable LOCALS=new System.Collections.Generic.Dictionary<string,KitchenSink>()
-///map of global variables
-let mutable GLOBALS=new System.Collections.Generic.Dictionary<string,KitchenSink>()
-let mutable DEBUG=false
-let mutable PROCS=new System.Collections.Generic.Dictionary<string,Proc>()
+let culture=System.Globalization.CultureInfo.InvariantCulture
